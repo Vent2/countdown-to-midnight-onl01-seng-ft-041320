a5fa5cb7fed81716cefdeacd659cbb123d28dc8f
Gerald Ventura <ventura.c.gerald@gmail.com>
2020-04-14 19:57:45 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,10 +4,12 @@ number = 10
   puts "#{number} SECOND(S)!"
    number -= 1
  end
- while number <5 && number > 0
-   sleep 1
- end
  if number == 0
  "HAPPY NEW YEAR!"
  end
 end
+
+def countdown_with_sleep(countdown)
+  countdown.sleep 1
+end
+  
