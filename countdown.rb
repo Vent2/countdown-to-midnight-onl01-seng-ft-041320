11fe0ed6d3a544e72a33054157f678fed1cbfe7e
Gerald Ventura <ventura.c.gerald@gmail.com>
2020-04-14 19:46:15 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,8 +3,8 @@ number = 10
  while number > 0
   puts "#{number} SECOND(S)!"
    number -= 1
- end
- runtime = Benchmark.measure { countdown_with_sleep(5) }
+ when number == 5..1
+   sleep 1
  end
  if number == 0
  "HAPPY NEW YEAR!"
